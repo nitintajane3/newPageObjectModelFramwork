b1bb433b09c72a2d32edf6e69d58de0069296ee3
nitintajane3 <nitintajane3@gmail.com>
2019-04-01 18:29:02 +0530

    sdfk

--- eclipse-workspace/newpageObjectModel/src/main/java/com/ityss/newpageobject/helper/listener/RetryListener.java
+++ eclipse-workspace/newpageObjectModel/src/main/java/com/ityss/newpageobject/helper/listener/RetryListener.java
@@ -9,7 +9,8 @@ import org.testng.annotations.ITestAnnotation;
 
 public class RetryListener implements IAnnotationTransformer{
 
-	public void transform(ITestAnnotation arg0, Class arg1, Constructor arg2, Method arg3) {
+	public void transform(ITestAnnotation arg0, Class arg1, Constructor arg2, Method arg3) 
+	{
 		IRetryAnalyzer retry = arg0.getRetryAnalyzer();
 		if(retry == null){
 			arg0.setRetryAnalyzer(Retry.class);
