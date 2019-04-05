3be1640e839be36bb9cd417d4afa96c1edba34e9
nitintajane3 <nitintajane3@gmail.com>
2019-04-05 12:59:01 +0530

    dfjk

---
+++ automationpageObjectFramewrk/src/main/java/newframework/automationpageObjectFramewrk/App.java
@@ -0,0 +1,13 @@
+package newframework.automationpageObjectFramewrk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
